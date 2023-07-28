884407070001d703388941cac59dd3978c65090c
unknown <anafernandez.9217@gmail.com>
2023-07-28 08:56:45 -0600

    mi tarea

---
+++ Proyecto/Proyecto/Controllers/Examen1Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class Examen1Controller : Controller
+    {
+        private DBPRUEBASEntities db = new DBPRUEBASEntities();
+
+        // GET: Examen1
+        public ActionResult Index()
+        {
+            return View(db.Examen.ToList());
+        }
+
+        // GET: Examen1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Examen examen = db.Examen.Find(id);
+            if (examen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(examen);
+        }
+
+        // GET: Examen1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Examen1/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idExamen,Nombre,Descripcion")] Examen examen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Examen.Add(examen);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(examen);
+        }
+
+        // GET: Examen1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Examen examen = db.Examen.Find(id);
+            if (examen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(examen);
+        }
+
+        // POST: Examen1/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idExamen,Nombre,Descripcion")] Examen examen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(examen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(examen);
+        }
+
+        // GET: Examen1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Examen examen = db.Examen.Find(id);
+            if (examen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(examen);
+        }
+
+        // POST: Examen1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Examen examen = db.Examen.Find(id);
+            db.Examen.Remove(examen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proyecto/Proyecto/Controllers/ExamenController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class ExamenController : Controller
+    {
+        // GET: Examen
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Listar()
+        {
+
+            List<Examen> oLstExamen = new List<Examen>();
+
+            using (DBPRUEBASEntities db = new DBPRUEBASEntities()) {
+
+                oLstExamen = (from p in db.Examen
+                               select p).ToList();
+            }
+            return Json(new { data = oLstExamen }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Obtener(int idexamen) {
+            Examen oExamen = new Examen();
+
+            using (DBPRUEBASEntities db = new DBPRUEBASEntities()) {
+
+                oExamen = (from p in db.Examen.Where(x => x.idExamen == idexamen)
+                            select p).FirstOrDefault();
+            }
+
+            return Json(oExamen, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Guardar(Examen oExamen) {
+
+            bool respuesta = true;
+            try
+            {
+
+                if (oExamen.idExamen == 0)
+                {
+                    using (DBPRUEBASEntities db = new DBPRUEBASEntities())
+                    {
+                        db.Examen.Add(oExamen);
+                        db.SaveChanges();
+                    }
+                }
+                else
+                {
+                    using (DBPRUEBASEntities db = new DBPRUEBASEntities())
+                    {
+                        Examen tempExamen = (from p in db.Examen
+                                                where p.idExamen == oExamen.idExamen
+                                                select p).FirstOrDefault();
+
+                        tempExamen.Nombre = oExamen.Nombre;
+                        tempExamen.Descripcion = oExamen.Descripcion;
+
+                        db.SaveChanges();
+                    }
+
+                }
+            }
+            catch {
+                respuesta = false;
+
+            }
+
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult Eliminar(int idexamen)
+        {
+            bool respuesta = true;
+            try
+            {
+                using (DBPRUEBASEntities db = new DBPRUEBASEntities())
+                {
+                    Examen oExamen = new Examen();
+                    oExamen = (from p in db.Examen.Where(x => x.idExamen == idexamen)
+                                select p).FirstOrDefault();
+
+                    db.Examen.Remove(oExamen);
+
+                    db.SaveChanges();
+                }
+            }
+            catch {
+                respuesta = false;
+            }
+
+            
+
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
